df4b3a201d82409a4d7cb161a5e03b9a65bf418b
Vipin Kashyap <mrvipinkashyap@users.noreply.github.com>
2018-02-20 23:22:30 +0530

    Initial commit for demo

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+declare var cordova;
+declare var window;
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = HomePage;
+
+  pages: Array<{ title: string, component: any }>;
+
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+    window.skipLocalNotificationReady = true
+    // used for an example of ngFor and navigation
+    this.pages = [
+    ];
+    this.platform.ready().then(() => {
+      this.initializeApp();
+    })
+  }
+
+  initializeApp() {
+    // Okay, so the platform is ready and our plugins are available.
+    // Here you can do any higher level native things you might need.
+    this.statusBar.styleDefault();
+    this.splashScreen.hide();
+    cordova.plugins.notification.local.on('click', function (obj) {
+      console.log("obj on: ", obj);
+      alert("Alert: " + JSON.stringify(obj));
+    });
+    cordova.plugins.notification.local.fireQueuedEvents();
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LocalNotifications } from '@ionic-native/local-notifications';
+import * as moment from 'moment';
+declare var cordova;
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  cordova: any;
+  notificationForm: FormGroup;
+  constructor(public navCtrl: NavController, public platform: Platform, public formBuilder: FormBuilder,
+    private localNotifications: LocalNotifications
+  ) {
+    this.notificationForm = formBuilder.group({
+      id: ['', Validators.required],
+    })
+  }
+
+  submit() {
+    var vm = this;
+    let addnotifications = () => {
+      console.log("addnotifications");
+    }
+ 
+    cordova.plugins.notification.local.schedule(
+      {
+        id: Math.floor(Math.random() * 10000),
+        title: 'My first notification',
+        trigger: {at: (new Date().getTime() + 3600)},
+      }
+    );
+  }
+  
+}
